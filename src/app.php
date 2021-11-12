e0ecd021bffea94405dfcdf06feee3f7302889db
yt2951 <rahman.yusuftaufiq@gmail.com>
2021-11-12 16:31:25 +0700

    The separation of concerns

--- src/app.php
+++ src/app.php
@@ -1,42 +1,9 @@
 <?php
 
-// https://symfony.com/doc/current/create_framework/routing.html
-
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-use Symfony\Component\HttpFoundation\Request;
-
-function is_leap_year($year = null)
-{
-    if (null === $year) {
-        $year = date('Y');
-    }
-
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
+use Calendar\Controller\LeapYearController;
 
 $routes = new Routing\RouteCollection();
-
-$routes->add('hello', new Routing\Route('/hello/{name}', [
-    'name' => 'World',
-    '_controller' => 'render_template',
-]));
-
-$routes->add('bye', new Routing\Route('/bye', [
-    '_controller' => 'render_template',
-]));
-
-class LeapYearController
-{
-    public function index(Request $request, $year)
-    {
-        if (is_leap_year($year)) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
-}
 
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
     'year' => null,
--- web/front.php
+++ web/front.php
@@ -1,7 +1,5 @@
 <?php
 
-// https://symfony.com/doc/current/create_framework/http_foundation.html
-
 // xdebug_break();
 // php -dxdebug.mode=debug -dxdebug.start_with_request=yes -S localhost:8080 web/front.php
 // eval(\Psy\sh());
@@ -9,40 +7,20 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
 use Symfony\Component\Routing;
-
-function render_template(Request $request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__ . '/../src/app.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
 
-// try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-// } catch (Routing\Exception\ResourceNotFoundException $exception) {
-//     $response = new Response('Not Found', 404);
-// } catch (Exception $exception) {
-//     $response = new Response('An error occurred', 500);
-// }
+$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->send();
