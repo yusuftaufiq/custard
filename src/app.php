e7955d7697f1a4a368621e40cfdfb478a11c13d5
yt2951 <rahman.yusuftaufiq@gmail.com>
2021-11-12 15:41:20 +0700

    Create HTTP kernel controller resolver

--- src/app.php
+++ src/app.php
@@ -4,6 +4,7 @@
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Request;
 
 function is_leap_year($year = null)
 {
@@ -25,15 +26,21 @@ $routes->add('bye', new Routing\Route('/bye', [
     '_controller' => 'render_template',
 ]));
 
-$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
-    'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
+class LeapYearController
+{
+    public function index(Request $request, $year)
+    {
+        if (is_leap_year($year)) {
             return new Response('Yep, this is a leap year!');
         }
 
         return new Response('Nope, this is not a leap year.');
     }
+}
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => [LeapYearController::class, 'index'],
 ]));
 
 return $routes;
