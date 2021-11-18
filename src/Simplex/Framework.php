4f7de67686f62f8deb467ab80ab4b639ffff2c59
yt2951 <rahman.yusuftaufiq@gmail.com>
2021-11-18 15:14:05 +0700

    Wrap front controller into framework

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,12 +2,40 @@
 
 namespace Simplex;
 
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\{Request, Response, RequestStack};
+use Symfony\Component\HttpKernel\Controller\{ArgumentResolver, ControllerResolver};
+use Symfony\Component\HttpKernel\EventListener\{ErrorListener, ResponseListener, RouterListener};
 use Symfony\Component\HttpKernel\{HttpKernel, HttpKernelInterface};
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\{Matcher\UrlMatcher, RequestContext, RouteCollection};
 
 class Framework extends HttpKernel
 {
+    public function __construct(RouteCollection $routes)
+    {
+        $context = new RequestContext();
+        $matcher = new UrlMatcher($routes, $context);
+        $requestStack = new RequestStack();
+
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $dispatcher = new EventDispatcher();
+
+        $dispatcher->addSubscriber(new ContentLengthListener());
+        $dispatcher->addSubscriber(new GoogleListener());
+
+        $dispatcher->addSubscriber(new ErrorListener(fn (FlattenException $exception): Response => (
+            new Response("Something went wrong! ({$exception->getMessage()})", $exception->getStatusCode())
+        )));
+        $dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
+        $dispatcher->addSubscriber(new ResponseListener('UTF-8'));
+        $dispatcher->addSubscriber(new StringResponseListener());
+
+        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+    }
+
     public function handle(
         Request $request,
         int $type = HttpKernelInterface::MAIN_REQUEST,
--- web/front.php
+++ web/front.php
@@ -2,42 +2,16 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\{Request, RequestStack};
-use Symfony\Component\Routing\{RequestContext, Matcher\UrlMatcher};
-use Symfony\Component\HttpKernel\Controller\{ArgumentResolver, ControllerResolver};
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\ErrorHandler\Exception\FlattenException;
-use Symfony\Component\HttpFoundation\Response;
 use Simplex\Framework;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpCache\{HttpCache, Store};
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
 $routes = include __DIR__ . '/../src/app.php';
 
-$context = new RequestContext();
-$matcher = new UrlMatcher($routes, $context);
-
-$dispatcher = new EventDispatcher();
-
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-$dispatcher->addSubscriber(new Simplex\GoogleListener());
-// $dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener(Calendar\Controller\ErrorController::class . '::exception'));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener(fn (FlattenException $exception): Response => (
-    new Response("Something went wrong! ({$exception->getMessage()})", $exception->getStatusCode())
-)));
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-// $framework = new Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-
-$framework = new HttpKernel\HttpCache\HttpCache(
-    new Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver),
-    new HttpKernel\HttpCache\Store(__DIR__ . '/../cache'),
+$framework = new HttpCache(
+    new Framework($routes),
+    new Store(__DIR__ . '/../cache'),
 );
 
-$response = $framework->handle($request);
-$response->send();
+$framework->handle($request)->send();
