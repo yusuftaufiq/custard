78cc7d77d78ed990e23bb518ec7c510c44a27a19
yt2951 <rahman.yusuftaufiq@gmail.com>
2021-12-26 21:10:42 +0700

    Add database migration

---
+++ database/migrations/20211226053634_create_activities_table.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateActivitiesTable extends AbstractMigration
+{
+    public function change(): void
+    {
+        $this->table('activities')
+            ->addColumn('email', 'string', [
+                'null' => true,
+            ])
+            ->addColumn('title', 'string')
+            ->addColumn('created_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+            ])
+            ->addColumn('updated_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'update' => 'CURRENT_TIMESTAMP',
+            ])
+            ->addColumn('deleted_at', 'timestamp', [
+                'null' => true,
+            ])
+            ->create();
+    }
+}
---
+++ database/migrations/20211226073428_create_todo_lists_table.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateTodoListsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $this->table('todo_lists')
+            ->addColumn('activity_group_id', 'integer')
+            ->addForeignKey('activity_group_id', 'activities', 'id', [
+                'delete' => 'CASCADE',
+                'update' => 'NO_ACTION',
+            ])
+            ->addColumn('title', 'string')
+            ->addColumn('is_active', 'boolean', [
+                'default' => true,
+                'null' => true,
+            ])
+            ->addColumn('priority', 'enum', [
+                'values' => [
+                    "very-low",
+                    "low",
+                    "high",
+                    "very-high",
+                ],
+            ])
+            ->addColumn('created_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+            ])
+            ->addColumn('updated_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'update' => 'CURRENT_TIMESTAMP',
+            ])
+            ->addColumn('deleted_at', 'timestamp', [
+                'null' => true,
+            ])
+            ->create();
+    }
+}
