7409f007b84fe763d44e1f4e0744c1040af420f4
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-01-02 20:03:47 +0700

    Updated benchmark test

--- benchmark/run.js
+++ benchmark/run.js
@@ -1,75 +1,50 @@
 const autocannon = require('autocannon');
 
-const API_URL = 'http://localhost:8008';
+const API_URL = 'http://localhost:8090';
 const WORKER_THREADS = 1;
-const CONCURRENT_CONNECTIONS = 20;
-const CONNECTION_DURATION_IN_SECOND = 20;
-const CONNECTION_TIMEOUT_IN_MILLISECOND = 2000;
+const CONCURRENT_CONNECTIONS = 10;
+const NUMBER_OF_REQUESTS = 1000;
+const CONNECTION_TIMEOUT_IN_MILLISECOND = 1000;
 
 const CONFIGS = {
   url: API_URL,
   workers: WORKER_THREADS,
   connections: CONCURRENT_CONNECTIONS,
-  duration: CONNECTION_DURATION_IN_SECOND,
+  amount: NUMBER_OF_REQUESTS,
   timeout: CONNECTION_TIMEOUT_IN_MILLISECOND,
 };
 
 (async () => {
-  const insertActivity = await autocannon({
+  const insertTodoList = autocannon({
     ...CONFIGS,
     requests: [{
       method: 'POST',
-      path: '/activity-groups',
+      path: '/todo-items',
       headers: {
         'Content-type': 'application/json;'
       },
       body: JSON.stringify({
-        title: 'Hello World!',
-        email: 'new-[<id>]@user.com',
+        activity_group_id: 1,
+        title: 'Task-[<id>]',
       })
     }],
     idReplacement: true,
   });
 
-  const showActivities = await autocannon({
+  const showTodoLists = autocannon({
     ...CONFIGS,
     requests: [{
       method: 'GET',
-      path: '/activity-groups',
+      path: '/todo-items',
       headers: {
         'Content-type': 'application/json;'
       },
     }],
   });
 
-  const showActivity = await autocannon({
-    ...CONFIGS,
-    requests: [{
-      method: 'GET',
-      path: '/activity-groups/1',
-      headers: {
-        'Content-type': 'application/json;'
-      },
-    }],
-  });
+  const testsCase = await Promise.all([insertTodoList, showTodoLists]);
 
-  const updateActivity = await autocannon({
-    ...CONFIGS,
-    requests: [{
-      method: 'PATCH',
-      path: '/activity-groups/1',
-      headers: {
-        'Content-type': 'application/json;'
-      },
-      body: JSON.stringify({
-        title: 'Hello World!',
-        email: 'update-[<id>]@user.com',
-      })
-    }],
-    idReplacement: true,
-  });
-
-  const benchmarksResult = [insertActivity, showActivities, showActivity, updateActivity].reduce((result, value) => ({
+  const benchmarksResult = testsCase.reduce((result, value) => ({
     failed: (result.failed + value.non2xx) / 2,
     latencyAverage: (result.latencyAverage + value.latency.average) / 2,
     requestsAverage: (result.requestsAverage + value.requests.average) / 2,
@@ -79,5 +54,5 @@ const CONFIGS = {
   console.log(`Requests failed: ${benchmarksResult.failed}`);
   console.log(`Average latency: ${benchmarksResult.latencyAverage} ms`);
   console.log(`Average req/sec: ${benchmarksResult.requestsAverage}`);
-  console.log(`Average bytes/sec: ${benchmarksResult.latencyAverage} MB`);
+  console.log(`Average bytes/sec: ${benchmarksResult.throughputAverage} MB`);
 })();
