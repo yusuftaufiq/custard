3beb82ac1866852af59aaca6ac8132e17238c766
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-01-06 08:44:15 +0700

    Update code consistency

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -6,7 +6,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Activity;
 use App\Validators\ActivityValidator;
-use Core\Cache\Memcached;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,17 +13,14 @@ final class ActivityController
 {
     final public function index(Request $request): JsonResponse
     {
-        $memcached = new Memcached();
-        $result = $memcached->cache("{$request->getMethod()}:{$request->getUri()}", function () {
-            $activity = Activity::init();
-            $activities = $activity->all();
+        $activity = Activity::init();
+        $activities = $activity->all();
 
-            return json_encode([
-                'status' => 'Success',
-                'message' => 'OK',
-                'data' => $activities,
-            ]);
-        });
+        $result = json_encode([
+            'status' => 'Success',
+            'message' => 'OK',
+            'data' => $activities,
+        ]);
 
         $response = new JsonResponse();
         $response->setJson($result);
@@ -35,16 +31,13 @@ final class ActivityController
 
     final public function show(Request $request, int $id): JsonResponse
     {
-        $memcached = new Memcached();
-        $result = $memcached->cache("{$request->getMethod()}:{$request->getUri()}", function () use ($id) {
-            $activity = Activity::init();
+        $activity = Activity::init();
 
-            return json_encode([
-                'status' => 'Success',
-                'message' => 'OK',
-                'data' => $activity->find($id),
-            ]);
-        });
+        $result = json_encode([
+            'status' => 'Success',
+            'message' => 'OK',
+            'data' => $activity->find($id),
+        ]);
 
         $response = new JsonResponse();
         $response->setJson($result);
@@ -60,14 +53,17 @@ final class ActivityController
         ActivityValidator::validate()->store($requestActivity);
 
         $activity = Activity::init();
-
         $id = $activity->create($requestActivity);
 
-        $response =  new JsonResponse([
+        $result = json_encode([
             'status' => 'Success',
             'message' => 'Created',
             'data' => ['id' => $id] + $requestActivity + ['email' => null],
-        ], JsonResponse::HTTP_CREATED);
+        ]);
+
+        $response = new JsonResponse();
+        $response->setJson($result);
+        $response->setStatusCode(JsonResponse::HTTP_CREATED);
 
         return $response;
     }
@@ -78,11 +74,15 @@ final class ActivityController
 
         ActivityValidator::validate()->update($requestActivity);
 
-        $response =  new JsonResponse([
+        $result = json_encode([
             'status' => 'Success',
             'message' => 'OK',
             'data' => Activity::init()->update((int) $request->get('id', 0), $requestActivity),
-        ], JsonResponse::HTTP_OK);
+        ]);
+
+        $response = new JsonResponse();
+        $response->setJson($result);
+        $response->setStatusCode(JsonResponse::HTTP_OK);
 
         return $response;
     }
@@ -91,11 +91,15 @@ final class ActivityController
     {
         Activity::init()->delete((int) $request->get('id', 0));
 
-        $response =  new JsonResponse([
+        $result = json_encode([
             'status' => 'Success',
             'message' => 'OK',
             'data' => (object) [],
-        ], JsonResponse::HTTP_OK);
+        ]);
+
+        $response = new JsonResponse();
+        $response->setJson($result);
+        $response->setStatusCode(JsonResponse::HTTP_OK);
 
         return $response;
     }
--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -6,7 +6,6 @@ namespace App\Http\Controllers;
 
 use App\Models\TodoList;
 use App\Validators\TodoListValidator;
-use Core\Cache\Memcached;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -14,23 +13,20 @@ final class TodoListController
 {
     final public function index(Request $request): JsonResponse
     {
-        $memcached = new Memcached();
-        $result = $memcached->cache("{$request->getMethod()}:{$request->getUri()}", function () use ($request) {
-            $todoList = TodoList::init();
-            $todoLists = match ($request->get('activity_group_id', null)) {
-                null => $todoList->all(),
-                default => $todoList->find(
-                    column: 'activity_group_id',
-                    value: (int) $request->get('activity_group_id', 0),
-                ),
-            };
+        $todoList = TodoList::init();
+        $todoLists = match ($request->get('activity_group_id', null)) {
+            null => $todoList->all(),
+            default => $todoList->find(
+                column: 'activity_group_id',
+                value: (int) $request->get('activity_group_id', 0),
+            ),
+        };
 
-            return json_encode([
-                'status' => 'Success',
-                'message' => 'OK',
-                'data' => $todoLists,
-            ]);
-        });
+        $result = json_encode([
+            'status' => 'Success',
+            'message' => 'OK',
+            'data' => $todoLists,
+        ]);
 
         $response = new JsonResponse();
         $response->setJson($result);
@@ -41,16 +37,13 @@ final class TodoListController
 
     final public function show(Request $request, int $id): JsonResponse
     {
-        $memcached = new Memcached();
-        $result = $memcached->cache("{$request->getMethod()}:{$request->getUri()}", function () use ($id) {
-            $todoList = TodoList::init();
+        $todoList = TodoList::init();
 
-            return json_encode([
-                'status' => 'Success',
-                'message' => 'OK',
-                'data' => $todoList->find($id),
-            ]);
-        });
+        $result = json_encode([
+            'status' => 'Success',
+            'message' => 'OK',
+            'data' => $todoList->find($id),
+        ]);
 
         $response = new JsonResponse();
         $response->setJson($result);
@@ -72,11 +65,15 @@ final class TodoListController
         $newTodoList = ['id' => $id] + $requestTodoList + ['is_active' => 1, 'priority' => 'very-high'];
         $newTodoList['is_active'] = (bool) $newTodoList['is_active'];
 
-        $response =  new JsonResponse([
+        $result = json_encode([
             'status' => 'Success',
             'message' => 'Created',
             'data' => $newTodoList,
-        ], JsonResponse::HTTP_CREATED);
+        ]);
+
+        $response = new JsonResponse();
+        $response->setJson($result);
+        $response->setStatusCode(JsonResponse::HTTP_CREATED);
 
         return $response;
     }
@@ -87,11 +84,15 @@ final class TodoListController
 
         TodoListValidator::validate()->update($requestTodoList);
 
-        $response =  new JsonResponse([
+        $result = json_encode([
             'status' => 'Success',
             'message' => 'OK',
             'data' => TodoList::init()->update((int) $request->get('id', 0), $requestTodoList),
-        ], JsonResponse::HTTP_OK);
+        ]);
+
+        $response = new JsonResponse();
+        $response->setJson($result);
+        $response->setStatusCode(JsonResponse::HTTP_OK);
 
         return $response;
     }
@@ -100,11 +101,15 @@ final class TodoListController
     {
         TodoList::init()->delete((int) $request->get('id', 0));
 
-        $response =  new JsonResponse([
+        $result = json_encode([
             'status' => 'Success',
             'message' => 'OK',
             'data' => (object) [],
-        ], JsonResponse::HTTP_OK);
+        ]);
+
+        $response = new JsonResponse();
+        $response->setJson($result);
+        $response->setStatusCode(JsonResponse::HTTP_OK);
 
         return $response;
     }
